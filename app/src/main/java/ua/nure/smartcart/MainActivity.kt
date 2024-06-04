4831d71100753e283d01fdc18e8bd747d050b7a3
Andrii Yeremenko <andrii.yeremenko@teamdev.com>
2024-06-04 18:13:02 +0200

    Create skeleton of `SmartCart` app interface.

--- app/src/main/java/ua/nure/smartcart/MainActivity.kt
+++ app/src/main/java/ua/nure/smartcart/MainActivity.kt
@@ -2,15 +2,15 @@ package ua.nure.smartcart
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import ua.nure.smartcart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,8 +34,6 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
@@ -46,7 +44,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
--- app/src/main/java/ua/nure/smartcart/ui/gallery/CartGalleryFragment.kt
+++ app/src/main/java/ua/nure/smartcart/ui/gallery/CartGalleryFragment.kt
@@ -1,4 +1,4 @@
-package ua.nure.smartcart.ui.slideshow
+package ua.nure.smartcart.ui.gallery
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ua.nure.smartcart.databinding.FragmentSlideshowBinding
+import ua.nure.smartcart.databinding.FragmentGalleryBinding
 
-class SlideshowFragment : Fragment() {
+class CartGalleryFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentGalleryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,14 +20,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val cartGalleryViewModel =
+            ViewModelProvider(this).get(CartGalleryViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textGallery
+        cartGalleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
