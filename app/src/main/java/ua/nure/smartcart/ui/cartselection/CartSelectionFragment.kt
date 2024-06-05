f95941f7a3f5c59aa1235b921d07f7f276d18a18
Andrii Yeremenko <andrii.yeremenko@teamdev.com>
2024-06-05 02:42:51 +0200

    Create interface for `Carts` page.

--- app/src/main/java/ua/nure/smartcart/ui/cartselection/CartSelectionFragment.kt
+++ app/src/main/java/ua/nure/smartcart/ui/cartselection/CartSelectionFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ua.nure.smartcart.databinding.FragmentSlideshowBinding
@@ -13,8 +12,6 @@ class CartSelectionFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +25,6 @@ class CartSelectionFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
